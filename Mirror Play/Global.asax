2c6c6d33a98259543d98d4410e5182d4aaf41b50
Spencer Phillips <spencer@spencerkmedia.com>
2019-08-18 19:40:48 -0400

    Rebuilt VS Project with Authentication

--- Mirror Play/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mirror_Play.MvcApplication" Language="C#" %>
---
+++ MirrorPlay/Controllers/UController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MirrorPlay.Controllers
+{
+    public class UController : Controller
+    {
+        // GET: U
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
